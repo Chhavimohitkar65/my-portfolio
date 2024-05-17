fd6cf30f68de53c8ea655cf83a12d3eb83d49a29
Chhavimohitkar65 <chhavi.mohitkar06@gmail.com>
2024-05-17 18:27:40 +0530

    Fixed module path and updated dependencies

--- components/sub/SkillText.tsx
+++ components/sub/SkillText.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 
-const SkillText = ({ title, description, imageSrcs }) => {
+interface SkillTextProps {
+  title: string;
+  description: string;
+  imageSrcs: string[];
+}
+
+const SkillText: React.FC<SkillTextProps> = ({
+  title,
+  description,
+  imageSrcs,
+}) => {
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center">
       {/* Title */}
       <h1 className="text-white text-lg font-semibold mb-2">{title}</h1>
       {/* Content */}
-      <div className="bg-shadow-lg   bg-gradient-to-r from-purple-500 to-[#580a5f] rounded-lg shadow-md p-6 mb-6">
+      <div className="bg-shadow-lg bg-gradient-to-r from-purple-500 to-[#580a5f] rounded-lg shadow-md p-6 mb-6">
         {/* Images */}
         <div className="flex justify-center space-x-2 mb-4">
           {imageSrcs.map((src, index) => (
